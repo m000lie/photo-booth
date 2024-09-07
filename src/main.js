efec120bb295fd400f83294b76daf0cc5a8f9a54
m000lie <wong.shinzer@gmail.com>
2024-09-07 17:41:33 +0800

    added services and replaced image

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ function ariaExpanded () {
 
 document.addEventListener('scroll', e => {
   const scroll = document.documentElement.scrollTop
-  if (scroll >= 10) {
+  if (scroll >= 100) {
     document.querySelector('body').classList.add('scroll')
   } else {
     document.querySelector('body').classList.remove('scroll')
